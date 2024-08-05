35bd94ec60ad20867793835b315a03121be2bb50
Seng <ho.scheam@cma-cgm.com>
2024-08-05 17:22:24 +0200

    Ajout de contraintes sur le style

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ function App() {
   <div className="App">
     <Button bgColor='skyblue' color="black">Valider </Button> 
     <Button bgColor='skyblue' color='white' >Annuler </Button>
-    <Button className='yellow'><img src='/button.jpg' />NE PAS TOUCHER </Button>
+    <Button color='white' style={{textDecoration: 'underline', fontWeight:200}}><img src='/button.jpg' />NE PAS TOUCHER </Button>
   </div>
   );
 }
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -5,13 +5,24 @@ import style from './Button.module.css';
 const Button = (props) => {
   console.log(props);
     return (
-    <div style={{backgroundColor:props.bgColor, color: props.color}} className={style.Button} data-testid="Button">{props.children}</div>
+    <div style={{...props.style, backgroundColor: props.bgColor, color: props.color}} className={style.Button} data-testid="Button">{props.children}</div>
   );
 };
 
 Button.propTypes = {
     bgColor: PropTypes.string.isRequired,
-    color: PropTypes.oneOf(['black', 'white']).isRequired
+    color: PropTypes.oneOf(['black', 'white']).isRequired,
+    style : PropTypes.exact(
+    {
+        fontWeight: PropTypes.oneOf([100, 500,900]),
+        textDecoration: PropTypes.oneOf(['underline', 'none']),
+    })
+};
+
+Button.defaultProps = {
+    bgColor: 'yellow',
+    // color: 'black',
+    // style: {fontWeight: 100, textDecoration: 'none'}
 };
 
 export default Button;
