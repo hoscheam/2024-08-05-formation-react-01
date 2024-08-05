93c3bc8076d7c385d60b05719c751e771bab1fe4
Seng <ho.scheam@cma-cgm.com>
2024-08-05 17:04:02 +0200

    controles avec PropTypes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,9 +5,9 @@ import Button from '../ui/Button/Button';
 function App() {
   return (
   <div className="App">
-    <Button>Valider </Button> 
-    <Button>Annuler </Button>
-    <Button style={{textDecoration: 'underline'}} bgColor="skyblue" color="yellow"><img src='/button.jpg' />NE PAS TOUCHER </Button>
+    <Button bgColor='skyblue' color="black">Valider </Button> 
+    <Button bgColor='skyblue' color='white' >Annuler </Button>
+    <Button className='yellow'><img src='/button.jpg' />NE PAS TOUCHER </Button>
   </div>
   );
 }
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -1,11 +1,17 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import style from './Button.module.css';
 
 const Button = (props) => {
   console.log(props);
     return (
-    <div style={{...props.style, backgroundColor:props.bgColor, color: props.color}} className={style.Button} data-testid="Button">{props.children}</div>
+    <div style={{backgroundColor:props.bgColor, color: props.color}} className={style.Button} data-testid="Button">{props.children}</div>
   );
+};
+
+Button.propTypes = {
+    bgColor: PropTypes.string.isRequired,
+    color: PropTypes.oneOf(['black', 'white']).isRequired
 };
 
 export default Button;
